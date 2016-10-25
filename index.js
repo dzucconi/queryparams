1547ef8a2722f53df541fae0f98b321cdd5a9c5a
dzucconi <mail@damonzucconi.com>
2016-10-25 13:22:14 +0200

    Return the value

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var queryparams = require('./lib/queryparams');
 
 module.exports = function(defaults) {
-  queryparams(window.location.search.slice(1), defaults || {});
+  return queryparams(window.location.search.slice(1), defaults || {});
 };
